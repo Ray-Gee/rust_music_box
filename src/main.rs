0cea0ca0fa1c25876b4305b19e8487eefb7800d7
UedaRyuichi <uedaryuichi.sales@gmail.com>
2021-09-25 16:57:35 +0900

    1, 38分

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rspotify;
+
+use std::env;
+use std::collections::HashMap;
+
+use rspotify::blocking::client::Spotify;
+use rspotify::blocking::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
+use rspotify::senum::Country;
+use dotenv::dotenv;
+
+type Result<T> = std::result::Result<T,Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    dotenv().ok();
+
+    let env_vars = env::vars().collect::<HashMap<_, _>>();
+
+    for (key, _value) in env_vars.iter() {
+        println!("{:?}", key)
+    }
+
+    let client_credential = SpotifyClientCredentials::default().client_id();
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .build();
+
+    match spotify.me() {
+        Ok(user_profile) => println!("{:?}", user_profile),
+        Err(error) => println!("{:?}", error),
+    }
+
+    // let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
+    // let tracks = spotify.artist_top_tracks(birdy_uri, Country::UnitedStates)?;
+
+    // println!("{:?}", tracks);
+
+    Ok(())
 }
